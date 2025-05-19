51a72b1e49ec0c30f038ec5cb450c622a9f4fbf1
Luca Raveri <lraveri@plank-dev.systems>
2025-05-19 07:42:02 +0000

    RELEASE

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # Plank Statistics
+
+### Release su dev
+
+npm run build
+
+### Release in prod
+
+npm run release
+
+commit and push
+
+git tag vx.x.x
+
+git push origin vx.x.x
+
+modificare la versione in snippet PHP
+
+commit e push in PageEvents
--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,18 @@ import { Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import StatPage from './pages/StatPage';
 import ScrollToTop from './components/ScrollToTop';
+import { useEffect } from 'react';
 
 function App() {
+  useEffect(() => {
+    const handleClick = () => {
+      window.parent.postMessage({ type: 'externalClick' }, '*');
+    };
+
+    document.addEventListener('click', handleClick);
+    return () => document.removeEventListener('click', handleClick);
+  }, []);
+
   return (
     <div>
       <ScrollToTop />
